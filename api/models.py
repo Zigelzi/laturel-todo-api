ac0d4dff7fdee9d5b0eae8b6c5e7c9c019b31f69
zigelzi <miika.a.savela@gmail.com>
2020-03-22 19:46:39 +0200

    Update Project model and start add_project route

--- api/models.py
+++ api/models.py
@@ -1,5 +1,12 @@
 from api import db, ma
+from datetime import datetime
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f'<Project {self.name} | {self.description} | {self.created_at}>'
--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,7 @@
 from flask import make_response, jsonify
+
 from api import app
+from api.models import Project
 
 @app.route('/api/sanity')
 def index():
@@ -7,5 +9,11 @@ def index():
         'status': 'success',
         'message': 'This is JSON from the backend!'
     }
+    json_object = jsonify(response_object)
+    return make_response(json_object, 200)
+
+@app.route('/api/project', methods=['POST'])
+def add_project():
+    response_object = {'status': 'success'}
     json_object = jsonify(response_object)
     return make_response(json_object, 200)
