a97af866d59a8628e934638bc8d1235da85b0e3b
zigelzi <miika.a.savela@gmail.com>
2020-03-27 19:28:29 +0200

    Update CORS

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ then
     echo "PostgreSQL started."
 fi
 
-if ["$FLASK_ENV"="development"]
+if [ "$FLASK_ENV" = "development" ]
 then
-    echo "Creating the database tables"
+    echo "Creating the database tables..."
     python manage.py create_db
     echo "Tables created"
 fi
