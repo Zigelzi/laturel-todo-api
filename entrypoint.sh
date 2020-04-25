6a9621ba8f5312f409fb6bdab23d208e4a99717b
zigelzi <miika.a.savela@gmail.com>
2020-04-25 15:40:06 +0300

    Add migrations to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ then
     done
 
     echo "PostgreSQL started."
+    echo "-------------------"
+    echo ""
 fi
 
 if [ "$FLASK_ENV" = "development" ]
@@ -16,12 +18,19 @@ then
     echo "Creating the database tables..."
     python manage.py create_db
     echo "Tables created"
+    echo "-------------------"
+    echo ""
 
-    # echo "Performing any database migrations..."
-    # python manage.py db upgrade
-    # echo "Migrations completed successfully"
+    
 fi
 
+echo "Performing any database migrations..."
+python manage.py db  init
+python manage.py db migrate
+python manage.py db upgrade
+echo "Migrations completed successfully"
+echo "-------------------"
+echo ""
 
 
 exec "$@"
