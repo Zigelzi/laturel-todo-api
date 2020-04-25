9080af3fb3344c12100377991749a0f666cbe1b1
zigelzi <miika.a.savela@gmail.com>
2020-04-25 16:37:45 +0300

    Upgrade Flask and update env files for stage/dev

--- api/__init__.py
+++ api/__init__.py
@@ -4,10 +4,20 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-from api.config import DevConfig, Config
+from api.config import DevConfig, StageConfig, Config, todo_env
 
 app = Flask(__name__)
-app.config.from_object(DevConfig)
+
+if todo_env == 'development':
+    print('Running in TODO development mode...')
+    app.config.from_object(DevConfig)
+elif todo_env == 'stage':
+    print('Running in TODO staging mode...')
+    app.config.from_object(StageConfig)
+else:
+    print('TODO environment was not found. Please review the configuration variables.')
+    print (f'todo_env is {todo_env}')
+    app.config.from_object(Config)
 
 # Enable CORS for all resources
 CORS(app, resources={r'/*': {'origins': '*'}})
